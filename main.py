5edcfa2d1cfd9e98161f76d2748f9386df088ca5
vainmoon <serzh.umnov.93@mail.ru>
2026-02-03 00:38:07 +0300

    hw1: add router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
-
+from routers.moderation import router as moderation_router
 
 app = FastAPI()
 
@@ -8,6 +8,7 @@ app = FastAPI()
 async def root():
     return {'message': 'Hello World'}
 
+app.include_router(moderation_router, prefix='/moderation')
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8003)
